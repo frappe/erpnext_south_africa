325b474082a22620935e92f9a18a8e5f071add94
Diksha Jadhav <dikshajadhav11.dj@gmail.com>
2022-02-18 08:07:40 +0530

    move south africa vat account doctype from accounts and add it here

---
+++ erpnext_south_africa/erpnext_south_africa/doctype/south_africa_vat_account/south_africa_vat_account.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Frappe Technologies Private Limited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class SouthAfricaVATAccount(Document):
+	pass
